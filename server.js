6839b7b26cbbc291f9a64b83fa05146c29509c4c
ishal <muhammedishal0483@gmail.com>
2022-11-07 14:44:45 +0530

    cors_solving

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-// app.use(cors());
-app.use(
-  cors({
-    origin: [ 'http://localhost:6600', 'http://localhost:3000' ],
-    credentials: true,
-    origin: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 dotenv.config();
 app.use(express.json());
 app.use(
@@ -22,6 +18,7 @@ app.use(
     useTempFiles: true,
   })
 );
+app.use(cors())
 
 
 //routes
